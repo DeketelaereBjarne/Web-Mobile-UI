08168365cba56ad5c22b8f4ea185e9b6173ddae3
frozenkid <bjarne.deketelaere@student.howest.be>
2017-05-22 20:25:57 +0200

    Added localStorage Added Css Added bootstrap into html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
 /**
  * Created by Bjarne Deketelaere on 22/05/2017.
  */
+var orders=[];
+
+var processForm=function(e){
+    e.preventDefault();
+    var order={};
+    order.firstname=$("#firstname").val();
+    order.lastname=$("#lastname").val();
+    order.email=$("#email").val();
+    order.description=$("#details").val();
+    orders.push(order);
+    opslaanItem();
+
+};
+
+
+var opslaanItem=function(){
+    if((typeof Storage)!==void(0)){
+        localStorage.setItem("Orders",JSON.stringify(orders));
+    }
+};
+var ophalenItem=function(){
+    if((typeof Storage)!==void(0) && localStorage.getItem("Orders")!==null){
+        orders=JSON.parse(localStorage.getItem("Orders"));
+    }
+};
+
+$(document).ready(function(){
+    ophalenItem();
+    $("#submit").on("click",processForm);
+});
